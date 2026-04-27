233c5da0cda6e276a49f68a67bc4f74da8c71c34
kennyhemmy1 <tharealkenny07@gmail.com>
2026-04-27 03:13:42 -0400

    Create dashboard-nav.tsx

---
+++ components/dashboard-nav.tsx
@@ -0,0 +1,8 @@
+export default function DashboardNav() {
+  return (
+    <nav className="bg-zinc-950 border-b p-4 flex justify-between">
+      <div className="font-black text-3xl text-green-500">SITEPROOF</div>
+      <div>TRUSTED BY 1,847 CONTRACTORS</div>
+    </nav>
+  );
+}
